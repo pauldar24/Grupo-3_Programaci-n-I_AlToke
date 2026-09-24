9471079d1ec38121168e70ed8c811851538ca951
Grupo-3 AlToke <grupo3@altoke.edu>
2026-09-24 00:35:46 -0500

    feat(paso4): configurar sesiones y caché distribuido con Redis

--- obj/Debug/net10.0/GRUPAL.AssemblyInfo.cs
+++ obj/Debug/net10.0/GRUPAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GRUPAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dad494f52b566bb60eca515dc6939b897f0d62cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc3ac32d61611f811be95ac7231098240cc1b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GRUPAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GRUPAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
